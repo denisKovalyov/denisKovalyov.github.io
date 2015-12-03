ee1e397fba9ef0507515f0a920149de9fce68d60
Denis Kovalyov <kovalovdenis@gmail.com>
2015-12-04 01:20:50 +0200

    Exam work: 320px completed/ JS_HW_1-2 completed

--- GoIT/javascript/lesson 1-2/js/script.js
+++ GoIT/javascript/lesson 1-2/js/script.js
@@ -2,69 +2,87 @@
 
 var x, n;
 
-function dataInput() {
+function exponentiation() {
+	dataEntering ();
+	validation();
+	pow (x,n);
+}
+
+function dataEntering() {
 	x = parseInt( prompt('Введите число для возведения в степень', '') );
-	
+	n = parseInt( prompt('Введите степень', '') );
+}
+
+function validation() {
 	if (isNaN(x)) {
 		do {
-			x = parseInt( prompt('Пожалуйста, введите числовое значение', '') );
+			x = parseInt( prompt('Пожалуйста, введите корректное значение числа для возведения в степень', '') );
 		} while (isNaN(x));
-	};
+	}
 
-	n = parseInt( prompt('Введите степень', '') );
-	
 	if (isNaN(n)) {
 		do {
-			n = parseInt( prompt('Пожалуйста, введите числовое значение', '') );
+			n = parseInt( prompt('Пожалуйста, введите числовое значение степени', '') );
 		} while (isNaN(n));
-	} 
-};
+	}
+}
 
 function pow(base, exponent) {
-	var result = 1;
+    var result = 1;
 
 	if (exponent > 0) {
 	for (i = 0; i < exponent; i++) {
 		result *=base;
-	};
+	}
 	} else {
 		for (i = 0; i > exponent; i--) {
 		result *=base;
-	}; 
+	}
 	result = 1/result;
-	};
-	console.log(base + ' в степени '+ exponent + ' равно: ', result)
-};
+	}
 
-function exponentiation () {
-	dataInput ();
-	pow (x,n);
-};
+	console.log(base + ' в степени '+ exponent + ' равно: ', result);
+}
+
 
 // Arrays and loops
 
-function arrayFilling () {
-	var array = [];
-	
+var array = [];
+
+var userName, flag = false;
+
+function unitedFunctionForUserNameCheck() {
+	arrayFilling();
+	userNameEntering();
+	determineFlag();
+	userNameCheck();
+}
+
+function arrayFilling() {
 	for (var i = 0; i < 5; i++) {
 		array.push( prompt('Введите имя № ' + (i+1), '') );
-	};
+	}
+}
 
-	var userName = prompt('Введите имя пользователя', '');
-	var flag = false;
+function userNameEntering() {
+	userName = prompt('Введите имя пользователя', '');
+}
 
+function determineFlag() {
 	for (var j = 0; j < array.length; j++) {
 		if (array[j] === userName) {
 			flag = true;
 			break;
-		};
-	};
+		}
+	}
+}
 
+function userNameCheck() {
 	if (flag) {
 			alert(userName + ', вы успешно вошли.');
 		} else {
 			alert('Извините, такого пользователя не существует.');
-	};
-};
+		}
+}
 
 
